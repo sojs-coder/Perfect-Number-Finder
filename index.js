715fb76d91495541f25c302a160255feb440cf67
SoJS coder <matfuller703@gmail.com>
2024-03-09 20:08:25 -0800

    added parameters, better output, terminated done workers

--- index.js
+++ index.js
@@ -5,53 +5,73 @@ const fs = require('fs');
 if (!fs.existsSync('./logs')) {
     fs.mkdirSync('./logs');
 }
+// read output.txt
+const output = fs.readFileSync('./output.txt', 'utf8');
 const writeStream = fs.createWriteStream(`./output.txt`);
+writeStream.write(output);
 const done = fs.createWriteStream(`./finished_works.txt`);
-function log(...args){
+function log(...args) {
     writeStream.write(args.join(' ') + '\n');
 }
-const maxWorkers = 16;
+log("=====================================");
+log("Testing for P values of 2^P - 1");
+log("Session started at " + new Date().toISOString());
+log("=====================================");
+const maxWorkers = parseInt(process.argv[2]) || 16;
+log(maxWorkers + " workers");
+const maxQueue = parseInt(process.argv[3]) || 1000;
+log(maxQueue + " max queue");
 let activeWorkers = 0;
 const queue = [];
 
 // Function to create a worker
-function createWorker(data,i) {
-    console.log(i,"Worker created",data)
-    const worker = new Worker('./worker.js', { workerData: {
-        number: data,
-        i
-    } });
+function createWorker(data, i) {
+    console.log(i, "Worker created", data)
+    const worker = new Worker('./worker.js', {
+        workerData: {
+            number: data,
+            i
+        }
+    });
     activeWorkers++;
     worker.on('message', (message) => {
-        done.write(`WORKER (${message.index}) DONE: ${message.number}\n`);
+        done.write(`${message.index}\n`);
         if (message.isPrimeResult) {
-            log(message.number);
+            log("FOUND: " +message.number);
         }
-        activeWorkers--;
+        worker.terminate();
         if (queue.length > 0) {
-            const [nextData,i] = queue.shift();
-            createWorker(nextData,i);
+            const [nextData, i] = queue.shift();
+            createWorker(nextData, i);
         }
     });
 }
 
 // Test function for the main thread
-function test() {
-    log("Testing for P values of 2^P - 1");
-    log("-----------------------------")
-    for (let i = 200n; i < 1000; i++) {
+function loadQueue() {
+
+    // read from finished_works.txt
+    const finishedWorks = fs.readFileSync('./finished_works.txt', 'utf8').split('\n').map(Number);
+    // sort
+    finishedWorks.sort((a, b) => a - b);
+    // find the max
+    const max = finishedWorks[finishedWorks.length - 1];
+    // convert max to BigInt
+    const maxBigInt = BigInt(max);
+    log("Starting from  "+ maxBigInt + " to "+maxQueue+"...")
+    for (let i = maxBigInt; i < maxQueue; i++) {
         const data = (2n ** i) - 1n;
-        if (activeWorkers < maxWorkers) {
-            createWorker(data,i);
-        } else {
-            queue.push([data,i]);
-        }
+        queue.push([data, i]);
     }
 }
+
 
 // Main thread execution
 if (isMainThread) {
 
-    // Call the test function
-    test();
+    loadQueue();
+    for(var i = 0; i < maxWorkers; i++) {
+        const [data, index] = queue.shift();
+        createWorker(data, index);
+    }
 }
